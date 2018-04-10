3363ea9ffec5e890c909b3ad47a96ab13efb1164
Florian Pabich <florian.pabich@yahoo.fr>
2018-04-10 11:58:14 +0200

    add sections homePage

--- app/views/app/homePage.php
+++ app/views/app/homePage.php
@@ -1,26 +1,35 @@
-<div class="home-section1">
+<div class="home-showcase-title">
     <p>
         La Domotique vue par Domisep<br>
         Domisep est une entreprise qui permet un contrôle sécurisé et ergonomique de toute habitation<br>
         <a href="../404/"> En savoir plus</a>
     </p>
 </div>
-<section class="home-section2">
+<div class="home-sub-nav">
 
 
-    <img src="<?php echo new URL("img/logos/domisep_mini.png"); ?>" style="width: 50px;height: 50px" alt="logo" />
+    <img class="home-sub-nav-logo" src="<?php echo new URL("img/logos/domisep_mini.png"); ?>" style="width: 50px;height: 50px" alt="logo" />
 
-    <div class="home-item-menu">
+    <div class="home-sub-nav-item-menu">
         <a href="../404/" class="btn"><span class="home-item-menu">Fonctionnalités</span></a>
     </div>
-    <div class="home-item-menu">
+    <div class="home-sub-nav-item-menu">
         <a href="">
             S'inscrire
         </a>
     </div>
-    <div class="home-item-menu">
+    <div class="home-sub-nav-item-menu">
         <a href="">
             Nos Clients
         </a>
     </div>
-</section>
+</div>
+<div class="home-section">
+    Section1
+</div>
+<div class="home-section">
+    Section2
+</div>
+<div class="home-section">
+    Section3
+</div>
