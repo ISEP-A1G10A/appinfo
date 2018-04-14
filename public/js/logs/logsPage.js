7eba5417e4979e3cb0bfb2a06de0ffea252f25e1
Quentin Michel <quentinmichel69110@gmail.com>
2018-04-14 20:08:01 +0200

    little changes

--- public/js/logs/logsPage.js
+++ public/js/logs/logsPage.js
@@ -16,15 +16,8 @@ function onSearch() {
             var colum = colums[j];
             var search_value = $("#input_" + colum).val().toLowerCase();
             var box_value = $row.find(".logs-colum-" + colum).text().toLowerCase();
-            if (q === 1) {
-                console.log(search_value);
-                console.log(box_value);
-            }
             if (!box_value.includes(search_value)) {
                 shouldAppear = false;
-                if (q === 1) {
-                    console.log("hide");
-                }
             }
         }
         if (shouldAppear) {
