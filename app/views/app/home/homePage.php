5867bf9f481263bf325d36adf38533e5ddf1f3b0
amele <amele.nouna@isep.fr>
2018-05-03 11:45:54 +0200

    update section1

--- app/views/app/home/homePage.php
+++ app/views/app/home/homePage.php
@@ -18,27 +18,27 @@
     </div>
 </div>
 <div class="home-section">
-    <div class="home-section1-bloc1">
+    <div class="home-section1-bloc">
         <span class="home-bloc-title">Capter</span>
         <p>Notre catalogue de capteurs: choisissez, ajoutez et retirez des capteurs de votre maison</p>
     </div>
-    <div class="home-section1-bloc2">
+    <div class="home-section1-bloc">
         <span class="home-bloc-title">Transférer</span>
         <p>un transfert de données instantanné</p>
     </div>
-    <div class="home-section1-bloc1">
+    <div class="home-section1-bloc">
         <span class="home-bloc-title">Visualiser</span>
         <p>accédez et personnalisez votre maison</p>
     </div>
-    <div class="home-section1-bloc3">
+    <div class="home-section1-bloc">
         <span class="home-bloc-title">Economie</span>
         <p>Suivez votre consommation pour faire des économies </p>
     </div>
-    <div class="home-section1-bloc2">
+    <div class="home-section1-bloc">
         <span class="home-bloc-title">Sécurité</span>
         <p>La sécurité est un pilier fondateur de notre entreprise. Notre produit respecte la CNIL</p>
     </div>
-    <div class="home-section1-bloc4">
+    <div class="home-section1-bloc">
         <span class="home-bloc-title">Ecologie</span>
         <p>Quotidiennement nous relevons de nouveaux défis écologiques</p>
     </div>
