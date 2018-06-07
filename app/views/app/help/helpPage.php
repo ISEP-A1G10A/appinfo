7da795f9ad9afeb3ef2152890994908f39501fe5
Julie Palévody <jpalevody@gmail.com>
2018-06-07 09:19:54 +0200

    page help

--- app/views/app/help/helpPage.php
+++ app/views/app/help/helpPage.php
@@ -1,14 +1,16 @@
 <div class="help">
     <div class="help-tel-part">
+
+            <p><span class="help-intro"> Nous contacter par téléphone</span></p>
         <div class="help-tel-part-info">
             <div class="help-tel-part-info-2">
                 <div class="help-tel">
-                    <p><span class="help-tel-part-name">Notre numéro :</span></p> <span
+                    <p><span class="help-tel-part-name">Notre numéro </span></p> <span
                             class="help-telnumber">+33 01 55 45 40 90</span>
                 </div>
 
                 <div class="help-client">
-                    <p><span class="help-tel-part-name">Votre numéro client :</span></p> <span
+                    <p><span class="help-tel-part-name">Votre numéro client </span></p> <span
                             class="help-client-number">1945833786</span>
                 </div>
             </div>
@@ -16,10 +18,10 @@
         <div class="help-description">
             <div class="help-des-title">Comment procéder ?</div>
             <br>
-            <div>Appelez le numéro de téléphone.<br>
-                Précisez votre numéro client rappelé ci-contre.<br>
-                Indiquer l'objet de votre demande.<br>
-                Expliquez le problème rencontré.<br>
+            <div>1. Appelez le numéro de téléphone.<br>
+                2. Précisez votre numéro client rappelé ci-contre.<br>
+                3. Indiquer l'objet de votre demande.<br>
+                4. Expliquez le problème rencontré.<br>
             </div>
         </div>
 
@@ -55,9 +57,9 @@
             Quel est votre message ?
         </div>
         <br>
-        <div class="help-message-text">
-            Écrivez ici.
-        </div>
+        <textarea rows="10" cols="110" class="help-message-text">
+
+        </textarea>
 
         <br>
         <div class="help-send">
